54eaa219303eefc1c8ad9aaba4faf1c87599095c
Ricardo Canelas <ricardo.canelas@softwaredesign.ie>
2016-10-14 10:49:04 +0100

    removed sage theme

--- sites/helloworld/public/wp-app/themes/sage/assets/scripts/routes/SamplePage.js
+++
@@ -1,6 +0,0 @@
-export default {
-  init() {
-    // JavaScript to be fired on the sample page
-    console.log('- - JavaScript to be fired on the sample page - -');
-  },
-};
--- sites/helloworld/public/wp-app/themes/sage/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (defined('ABSPATH')) {
-    update_option('template', get_option('template') . '/templates');
-}
-die("Kind Regards,\nRoots");
